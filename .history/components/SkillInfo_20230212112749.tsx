a187e45c1a096d041ba57b9836718c0ce8e050c6
anhduc2708 <s3818988@rmit.edu.vn>
2023-02-12 15:31:19 +0700

    90%

---
+++ .history/components/SkillInfo_20230212112749.tsx
@@ -0,0 +1,31 @@
+import { motion } from 'framer-motion'
+import React from 'react'
+import {Skills  from './Skills'
+
+type Props = { 
+  skill: SkillType[]
+  directionLeft?: boolean;
+}
+
+function Skill({ title, url, directionLeft}: Props) {
+  return (
+    <div className='group relative flex cursor-pointer'>
+        <motion.img 
+        initial={{
+            x: directionLeft ? -200 :200,
+            opacity:0
+        }}
+        whileInView={{ opacity: 1, x:0}}
+        transition={{ duration:1}}
+        src={url}
+        className='rounded-full border border-gray-500 object-cover w-24 h-24  filter group-hover:grayscale transition duration-300 ease-in-out'/>
+    <div className="absolute opacity-0 group-hover:opacity-80 trasition duration-300 ease-in-out group-hover:bg-white h-24 w-24  rounded-full z-0">
+        <div className="flex items-center justify-center h-full">
+            <p className="text-3xl font-bold text-black opacity-100">{title}</p>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Skill
